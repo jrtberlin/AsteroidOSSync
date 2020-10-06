c5c18833e014b866fd1cf63bd50f743ad8283593
Justus Tartz <git@jrtberlin.de>
2020-10-06 13:06:57 +0200

    WIP: ble modularity

---
+++ app/src/main/java/org/asteroidos/sync/dataobjects/BleCharacteristic.java
@@ -0,0 +1,41 @@
+package org.asteroidos.sync.dataobjects;
+
+import org.asteroidos.sync.BuildConfig;
+
+import java.util.UUID;
+
+public class BleCharacteristic {
+    private String characteristicName;
+    private UUID characteristicUUID;
+    private boolean writeableCharacteristic;
+
+    public BleCharacteristic(String characteristicName, UUID characteristicUUID, boolean writeableCharacteristic) {
+        this.characteristicName = characteristicName;
+        this.characteristicUUID = characteristicUUID;
+        this.writeableCharacteristic = writeableCharacteristic;
+    }
+
+    public String getCharacteristicName() {
+        return characteristicName;
+    }
+
+    public UUID getCharacteristicUUID() {
+        return characteristicUUID;
+    }
+
+    public final boolean isWriteableCharacteristic() {
+        return this.writeableCharacteristic;
+    }
+
+    public final boolean isReadableCharactersitic(){
+        return !this.writeableCharacteristic;
+    }
+
+    public void printCharacteristicInfo() {
+        if (BuildConfig.DEBUG) {
+            System.out.println("BLE Characteristic " + getCharacteristicName() + "\n\tUUID: "
+                    + getCharacteristicUUID().toString() +
+                    "\n\tis writeable: " + isWriteableCharacteristic());
+        }
+    }
+}
---
+++ app/src/main/java/org/asteroidos/sync/dataobjects/BleService.java
@@ -0,0 +1,67 @@
+package org.asteroidos.sync.dataobjects;
+
+import androidx.annotation.Nullable;
+
+import java.util.UUID;
+
+public class BleService {
+    private final String serviceName;
+    private final UUID serviceUUID;
+    @Nullable
+    private BleCharacteristic[] serviceCharacteristics;
+
+    /***
+     *
+     * @param serviceName Name of the Service to display in debug functions
+     * @param serviceUUID {@link UUID} defining the BLE service
+     * @param serviceCharacteristics Array of {@link BleCharacteristic} that the service consists of
+     */
+    public BleService(String serviceName, UUID serviceUUID, @Nullable BleCharacteristic[] serviceCharacteristics) {
+        this.serviceName = serviceName;
+        this.serviceUUID = serviceUUID;
+        this.serviceCharacteristics = serviceCharacteristics;
+    }
+
+    public BleService(String serviceName, UUID serviceUUID) {
+        this.serviceName = serviceName;
+        this.serviceUUID = serviceUUID;
+        this.serviceCharacteristics = null;
+    }
+
+    public final void addCharacteristic(BleCharacteristic bleCharacteristic) {
+        int size = 0;
+        if (this.serviceCharacteristics != null) {
+            size = this.serviceCharacteristics.length + 1;
+        }
+
+        BleCharacteristic[] returnCharacteristic = new BleCharacteristic[size];
+        int i = 0;
+        if (this.serviceCharacteristics != null) {
+            for (BleCharacteristic characteristic : this.serviceCharacteristics) {
+                returnCharacteristic[i] = characteristic;
+                i += 1;
+            }
+        }
+        returnCharacteristic[i] = bleCharacteristic;
+        this.serviceCharacteristics = returnCharacteristic;
+    }
+
+    public final void addCharacteristics(BleCharacteristic[] characteristics) {
+        for (BleCharacteristic characteristic : characteristics) {
+            addCharacteristic(characteristic);
+        }
+    }
+
+    public final String getServiceName() {
+        return serviceName;
+    }
+
+    public final UUID getServiceUUID() {
+        return serviceUUID;
+    }
+
+    @Nullable
+    public final BleCharacteristic[] getServiceCharacteristics() {
+        return serviceCharacteristics;
+    }
+}
