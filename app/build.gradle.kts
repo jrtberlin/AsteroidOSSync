2f3d563b89bed95876df46dab731dacf8fd4fcb9
Justus Tartz <git@jrtberlin.de>
2024-08-27 10:50:44 +0200

    upgrade Android Material to 1.12.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.cardview:cardview:1.0.0")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("com.github.MagneFire:EasyWeather:1.3")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("org.osmdroid:osmdroid-android:6.1.16")
